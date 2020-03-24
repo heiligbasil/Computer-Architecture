b77a5dab53578870ed143265298911b47bce5334
heiligbasil <basilief@yahoo.com>
2020-03-23 17:22:58 -0700

    Add CPU instructions as class properties Implement run() method to interpret and execute program

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
@@ -10,35 +11,32 @@ class CPU:
         self.pc = 0
         self.reg = [0] * 8
         self.ram = [0 & 0xFF] * 8
+        self.HLT = 0b00000001
+        self.LDI = 0b10000010
+        self.PRN = 0b01000111
 
     def load(self):
         """Load a program into memory."""
-
         address = 0
-
         # For now, we've just hardcoded a program:
-
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
-
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -47,7 +45,6 @@ class CPU:
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
             #self.fl,
@@ -56,15 +53,28 @@ class CPU:
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
-
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
-
         print()
 
     def run(self):
         """Run the CPU."""
-        pass
+        # self.trace()
+        while self.pc < len(self.ram):
+            ir = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            if ir == self.HLT:
+                break
+            elif ir == self.LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif ir == self.PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            else:
+                print(f'Unknown instruction: {ir}')
+                exit()
 
     def ram_read(self, MAR):
         return self.ram[MAR]
