d670d505c61b0bbe4e32ce94a87e153b47c180de
heiligbasil <basilief@yahoo.com>
2020-03-25 14:11:38 -0700

    Modify reg/ram initial declaration to be '0' not 'bin(0)'

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,8 +9,8 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.pc = 0
-        self.reg = [bin(0)] * 8
-        self.ram = [bin(0)] * 256
+        self.reg = [0] * 8
+        self.ram = [0] * 256
         self.HLT = 0b00000001
         self.LDI = 0b10000010
         self.PRN = 0b01000111
